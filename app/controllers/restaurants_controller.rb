242edd5c13463efc859e0b59512e8ce384c99400
Gwenny Warnick <gwendolen.warnick@gmail.com>
2019-04-17 14:34:06 +1000

    added edit functionality, new routes, restaurant validation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,29 +1,43 @@
 class RestaurantsController < ApplicationController
   def index
-      @restaurants = Restaurant.all
+    @restaurants = Restaurant.all
   end
-
+  
   def create
-      @restaurant = Restaurant.new(restaurant_params)
-
-      if @restaurant.save
-          redirect_to @restaurant
-        else
-          render 'new'
-        end
-  end
-
-  def new
-      @restaurant = Restaurant.new
-  end
-
-  def show
-      @restaurant = Restaurant.find(params[:id])
-  end
-
-  private
-    def restaurant_params
-      params.permit(:title, :address, :description, :food_type)
-    end
+    @restaurant = Restaurant.new(restaurant_params)
     
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+  
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
+  end
+  
+  def new
+    @restaurant = Restaurant.new
+  end
+  
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+  
+  private
+  def restaurant_params
+    params.permit(:title, :address, :description, :food_type)
+  end
+  
 end
