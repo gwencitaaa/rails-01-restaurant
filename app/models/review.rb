f2f35c396b02ac5118dc97fe5095ff17c88790b6
Gwenny Warnick <gwendolen.warnick@gmail.com>
2019-04-17 14:42:24 +1000

    added review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,12 @@
-class Review < ApplicationRecord
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.string :content
+      t.integer :rating
+      t.belongs_to :restaurant, index: true
+
+      t.timestamps
+    end
+  end
 end
